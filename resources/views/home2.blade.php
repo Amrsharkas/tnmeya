14c9f7c8c707bf9f8dceaf9c741b6fe2b0da6777
Amr Sharkas <asharkas@arabiclocalizer.com>
2023-11-28 22:52:10 +0200

    fix

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -8,7 +8,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/css/style2.css">
+        <link rel="stylesheet" href="/css/style2.css?v=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script> 
     <script src="https://malsup.github.io/jquery.form.js"></script>
 
